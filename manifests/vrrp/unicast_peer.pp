506e846d9c838ac47d92fbbfea4c48ef95478314
dgibson <dgibson@rlsolutions.com>
2016-11-17 16:51:57 -0500

    Debugging exported unicast peers

--- manifests/vrrp/unicast_peer.pp
+++ manifests/vrrp/unicast_peer.pp
@@ -6,19 +6,20 @@
 # $instance:: Name of vrrp instance this peer belongs to
 #
 define keepalived::vrrp::unicast_peer (
+  $name = "${fqdn}_${title}",
   $instance,
 ) {
   $_inst = regsubst($instance, '[:\/\n]', '')
   $_name = regsubst($name, '[:\/\n\.]', '_')
   
-  validate_string( $name )
-  validate_ip_address( $name )
-  notify { "unicast_peer_${name}": message => "\nInstance=${instance}:\npeer=${name}:\n" }
+  validate_string( $title )
+  validate_ip_address( $title )
+  notify { "unicast_peer_${name}": message => "\nInstance=${instance}:\npeer=${title}:\n" }
   
-  if ! has_ip_address( $name ) {
+  if ! has_ip_address( $title ) {
     concat::fragment { "keepalived.conf_vrrp_instance_${_inst}_upeers_peer_${_name}":
       target  => "${::keepalived::config_dir}/keepalived.conf",
-      content => "    ${name}\n",
+      content => "    ${title}\n",
       order   => "100-${_inst}-020",
     }
   }
